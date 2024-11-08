f396396ee7a355b1867fac9d997dfc5611b10fe6
Barendan <barendan@gmail.com>
2024-11-07 19:21:38 -0500

    setup server and run it

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors');
+
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+
+app.use(cors())
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Reef API');
+  });
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+
+
